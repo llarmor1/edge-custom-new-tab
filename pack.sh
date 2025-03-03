43f59d8de3c55cea9e35449558fb99e9d632dff0
mardevour <mardevoir@proton.me>
2025-03-03 20:49:14 +0100

    updated: pack.sh README.md

--- pack.sh
+++ pack.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 # autmated compression into a zip file
-if [ ! -e "packed-latest.zip" ]; then
-    zip packed-latest.zip manifest.json src/*
-elif [ -e "packed-latest.zip" ]; then
-    mv packed-latest.zip "packed-$(date +%d-%m_%H-%M-%S).zip";
-    zip packed-latest.zip manifest.json src/*
+if  [ -e "packaged/" ]; then
+    echo "[$(date +%H:%M:%S)] directory already exists"
+elif [ ! -e "packaged/" ]; then
+    echo "[$(date +%H:%M:%S)] directory doesn't exist, creating it..."
+    mkdir packaged
+fi
+
+if [ ! -e "packaged/packaged-latest.zip" ]; then
+    echo "[$(date +%H:%M:%S)] no latest package found, packaging..."
+    zip packaged/packaged-latest.zip manifest.json src/*
+elif [ -e "packaged/packaged-latest.zip" ]; then
+    echo "[$(date +%H:%M:%S)] latest package found, renaming it"
+    echo "[$(date +%H:%M:%S)] compressing new latest package"
+    mv packaged/packaged-latest.zip "packaged/packaged-$(date +%d-%m_%H-%M-%S).zip";
+    zip packaged/packaged-latest.zip manifest.json src/*
+    echo "[$(date +%H:%M:%S)] created latest package in packaged/packaged-latest.zip"
 fi
